34b2ff61ba634d4ac11c7f5e180c2fbef21f6f26
Khanh Nguyen <khanhdaik134@gmail.com>
2020-12-27 18:46:07 +0700

    update query sneaker

--- controllers/SneakerController.js
+++ controllers/SneakerController.js
@@ -6,7 +6,7 @@ exports.index = (req, res) => {
   const limit = req.query.limit || 10;
   const offset = req.query.offset || 0;
   const sort = req.query.sort || 'DESC'
-  const orderBy = req.query.order || `sneakers.id ${sort}`;
+  const orderBy = req.query.order || `updated_at ${sort}`;
   const search = req.query.search;
   const brandId = req.query.brand;
   let searchObj = {};
