58704319bb334ce704d7f049d40e3cb74b1ca60c
Khanh Nguyen <khanhdaik134@gmail.com>
2020-12-27 18:50:58 +0700

    get logo brand

--- models/Sneaker.js
+++ models/Sneaker.js
@@ -10,7 +10,7 @@ class Sneaker extends Model {
   getAll(search, limit, offset, orderBy, callback){
     let orderByStr = orderBy ? `ORDER BY ${orderBy}` : '';
     let searchStr = search ? this.objectToString(search) : '';
-    let queryStr = `SELECT ${this.table}.*,brands.title as brand FROM ${this.table} INNER JOIN brands ON brands.id = ${this.table}.brand_id ${searchStr} ${orderByStr} LIMIT ${limit || 10} OFFSET ${offset || 0}`;
+    let queryStr = `SELECT ${this.table}.*,brands.title as brand, brands.logo FROM ${this.table} INNER JOIN brands ON brands.id = ${this.table}.brand_id ${searchStr} ${orderByStr} LIMIT ${limit || 10} OFFSET ${offset || 0}`;
     console.log(queryStr);
     this.getConnection().query(queryStr, function (res, err) {
       return callback(res, err);
