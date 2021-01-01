0402241180af6dc4ca471903d2414a2f1bbbf701
Khanh Nguyen <khanhdaik134@gmail.com>
2021-01-01 18:48:47 +0700

    update body form data

--- server.js
+++ server.js
@@ -1,16 +1,24 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const formidable = require('express-formidable');
+const os = require("os");
+const formData = require("express-form-data");
 const router = express.Router();
 const cors = require('cors')
 const app = express();
 const port = process.env.PORT || 3001;
 
+app.use(formData.parse({
+  uploadDir: os.tmpdir(),
+  autoClean: true
+}));
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
+
 
 app.use(cors());
 app.listen(port);
